2b1f5c23acbe95754a3e392119e5442589f625a3
Raschid Llamas <62359390+rshdd@users.noreply.github.com>
2024-01-29 09:58:30 -0600

    Added kernel.cu

---
+++ kernel.cu
@@ -0,0 +1,17 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+
+__global__ void print_hello_cuda() {
+    int i = threadIdx.x;
+    printf("[DEVICE] ThreadIdx.x: %d\n", i);
+}
+
+int main()
+{
+    print_hello_cuda << < 2, 8 >> > ();
+    return 0;
+}
