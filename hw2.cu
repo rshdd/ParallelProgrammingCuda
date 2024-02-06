c9e28ce102044fe6a727501e914cec07194314e1
Raschid Llamas <62359390+rshdd@users.noreply.github.com>
2024-02-05 21:06:29 -0800

    Add files via upload

---
+++ hw2.cu
@@ -0,0 +1,17 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void print_element()
+{
+    int globalID = (blockIdx.y * blockDim.y + threadIdx.y) * (gridDim.x * blockDim.x) + blockIdx.x * blockDim.x + threadIdx.x;
+    printf("[DEVICE] ThreadIsx.x: %d\n", globalID);
+}
+
+int main()
+{
+    print_element << <2, 8 >> > ();
+
+    return 0;
+}
